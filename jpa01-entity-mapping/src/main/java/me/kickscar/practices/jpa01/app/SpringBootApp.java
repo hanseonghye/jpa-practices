98bade42185a229e148af912f4e9ccb7b8e1ea75
kickscar <kickscar@gmail.com>
2019-10-13 15:12:31 +0900

    jpa01 모듈 Spring Boot App 이름들 변경

--- jpa01-entity-mapping/src/main/java/me/kickscar/practices/jpa01/app/SpringBootApp.java
+++ jpa01-entity-mapping/src/main/java/me/kickscar/practices/jpa01/app/SpringBootApp.java
@@ -14,7 +14,7 @@ import javax.persistence.PersistenceUnit;
 @SpringBootApplication
 // 엔티티 클래스 자동스캐닝 베이스 패키지 지정
 @EntityScan( basePackages = { "me.kickscar.practices.jpa01.domain" } )
-public class Jpa01SpringBootApp {
+public class SpringBootApp {
 
     //엔티티매니저팩토리 주입
     @PersistenceUnit
@@ -34,6 +34,6 @@ public class Jpa01SpringBootApp {
     }
 
     public static void main(String[] args) {
-        try(ConfigurableApplicationContext c = SpringApplication.run(Jpa01SpringBootApp.class, args)){}
+        try(ConfigurableApplicationContext c = SpringApplication.run(SpringBootApp.class, args)){}
     }
 }
