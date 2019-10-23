097f81370dc4f1b5884415f6bd896492056aa5cf
kickscar <kickscar@gmail.com>
2019-10-23 17:19:47 +0900

    Model3 JPA User Repository 코드 정리

--- jpa03-model03/src/main/java/me/kickscar/practices/jpa03/model03/repository/JpaUserRepository.java
+++ jpa03-model03/src/main/java/me/kickscar/practices/jpa03/model03/repository/JpaUserRepository.java
@@ -1,23 +1,8 @@
 package me.kickscar.practices.jpa03.model03.repository;
 
-import me.kickscar.practices.jpa03.model03.domain.GenderType;
-import me.kickscar.practices.jpa03.model03.domain.RoleType;
 import me.kickscar.practices.jpa03.model03.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface JpaUserRepository extends JpaRepository<User, Long>, JpaUserQryDslRepository {
     public User findByEmailAndPassword(String email, String password);
-
-    @Modifying
-    @Query("update User u set u.name=:name, u.email=:email, u.password=:password, u.gender=:gender, u.role=:role where u.no=:no")
-    public void update(
-            @Param("no") Long no,
-            @Param("name") String name,
-            @Param("email") String email,
-            @Param("password") String password,
-            @Param("gender") GenderType gender,
-            @Param("role") RoleType role);
 }
